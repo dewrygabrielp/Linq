302a3262a8beb001af849813318b54c36757c3d0
Dewry Peña <87697718+dewrygabrielp@users.noreply.github.com>
2022-04-05 13:52:37 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,7 +16,25 @@ namespace Linq
                 new Person(){Name= "Darlenis", Age = 22},
                 new Person(){Name= "Danilo", Age = 51}
             };
+            List<Person> lst1 = new List<Person>()
+             {
+                 new Person(){Name = "Vicenta", Age = 51},
+                 new Person(){Name= "Ramon", Age = 44},
+                 new Person(){Name= "Juan Pablo Duarte", Age = 44}
+             };
+            /*List<Pet> ListPet = new List<Pet>()
+            {
+                new Pet() {NamePet = "Nigga", Color = "Black"},
+                new Pet() {NamePet = "Bobby", Color="Brown"},
+                new Pet() {NamePet = "Tito", Color = "White"}
+            };*/
+            List<string> ListOrder = (from d in lst select d).Union(lst1).OrderBy(d => d.Age).Select(d => d.NameAndAge).ToList();
+            //IMPORTAN!
+            /* with this catch it will omit the first three data of the collection and it will only take the first one after the omitted three.
+to show all remove the skip and take methods*/
 
+            List<string> ListOrder1 = (from d in lst select d).Union(lst1).Skip(3).Take(1).OrderBy(d => d.Age).Select(d => d.NameAndAge).ToList();
+            /*
             List<string> ListPet = new List<string>()
             {
                  "Nigga - Black",
@@ -31,9 +49,9 @@ namespace Linq
                                       .Union(ListPet)
                                       .OrderBy(d=>d)
                                       .ToList();
+            */
 
-          
-            foreach(var item in OrderList)
+            foreach (var item in ListOrder)
             {
                 Console.WriteLine(item);
             }
